00d5fc6a55f4054f08eaa5d338106660a0e4a5d4
AryaNibras <aryanibras@gmail.com>
2022-10-09 16:20:57 +0700

    Mengubah isi file latihan1.php

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -8,7 +8,7 @@ class Latihan1 extends CI_Controller{
     public function penjumlahan($n1, $n2)
     {
         $this->load->model('model_latihan1');
-        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+        $hasil = $this->model_latihan1->jumlah($n1, $n2);
         echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
         .$hasil;
     }
