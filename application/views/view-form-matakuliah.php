ec7f80a02348b1f6268582589515721ac7929437
AryaNibras <aryanibras@gmail.com>
2022-10-09 23:07:41 +0700

    Menambahkan required pada file view-form-matakuliah.php

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -18,14 +18,16 @@
                 <th>Kode MTK</th>
                 <th>:</th>
                 <td>
-                    <input type="text" name="kode" id="kode">
+                    <input type="text" name="kode" id="kode" required oninvalid="this.setCustomValidity('Kode Matakuliah Harus diisi')" 
+                    oninput="this.setCustomValidity('')">
                 </td>
             </tr>
             <tr>
                 <th>Nama MTK</th>
                 <td>:</td>
                 <td>
-                    <input type="text" name="nama" id="nama">
+                    <input type="text" name="nama" id="nama" required oninvalid="this.setCustomValidity('Nama Matakuliah Harus diisi')"
+                    oninput="this.setCustomValidity('')">
                 </td>
             </tr>
             <tr>
@@ -48,5 +50,5 @@
         </table>
         </form>
         </center>
-    </body>
+</body>
 </html>
