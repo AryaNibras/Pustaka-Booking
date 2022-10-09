fde5db5cd4048d1eb6051545419eb8297eb8f86e
AryaNibras <aryanibras@gmail.com>
2022-10-10 01:57:58 +0700

    Menambahkan isi file view-form-matakuliah.php

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -18,23 +18,24 @@
                 <th>Kode MTK</th>
                 <th>:</th>
                 <td>
-                    <input type="text" name="kode" id="kode" required oninvalid="this.setCustomValidity('Kode Matakuliah Harus diisi')" 
-                    oninput="this.setCustomValidity('')">
+                    <input type="text" name="kode" id="kode" required oninvalid="this.setCustomValidity('Kode Matakuliah Wajib diisi minimal 3 angka')" 
+                    oninput="this.setCustomValidity('')" autocomplete="off" minlength="3" maxlength="7">
                 </td>
             </tr>
             <tr>
                 <th>Nama MTK</th>
                 <td>:</td>
                 <td>
-                    <input type="text" name="nama" id="nama" required oninvalid="this.setCustomValidity('Nama Matakuliah Harus diisi')"
-                    oninput="this.setCustomValidity('')">
+                    <input type="text" name="nama" id="nama" required oninvalid="this.setCustomValidity('Nama Matakuliah Wajib diisi')"
+                    oninput="this.setCustomValidity('')" autocomplete="off">
                 </td>
             </tr>
             <tr>
                 <th>SKS</th>
                 <td>:</td>
                 <td>
-                    <select name="sks" id="sks">
+                    <select name="sks" id="sks" required oninvalid="this.setCustomValidity('Pilih jumlah sks')"
+                    oninput="this.setCustomValidity('')">
                         <option value="">Pilih SKS</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
