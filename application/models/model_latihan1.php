d2457622757a4e065a653536bf406dbcbab88910
AryaNibras <aryanibras@gmail.com>
2022-10-09 16:07:02 +0700

    Mengubah isi file model_latihan1.php

--- application/models/model_latihan1.php
+++ application/models/model_latihan1.php
@@ -2,9 +2,9 @@
 class model_latihan1 extends CI_Model{
     public $nilai1, $nilai2, $hasil;
     public function jumlah($n1 = null, $n2 = null){
-        $this->nilai1 = $nil1;
-        $this->nilai2 = $nil2;
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
         $this->hasil = $this->nilai1 + $this->nilai2;
         return $this->hasil;
-        }
+    }
 }
